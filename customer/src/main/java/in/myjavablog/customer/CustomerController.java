45f76d571feee5789356c2397ffe29b2c8706d2e
Anup Bhagwat <anup.bhagwat7@gmail.com>
2023-04-02 12:20:49 +0530

    API gateway added for load balancing the external requests

--- customer/src/main/java/in/myjavablog/customer/CustomerController.java
+++ customer/src/main/java/in/myjavablog/customer/CustomerController.java
@@ -3,6 +3,7 @@ package in.myjavablog.customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping("/register")
+    @PostMapping
     public ResponseEntity<Customer> registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
 
         return ResponseEntity.ok(customerService.registerCustomer(customerRegistrationRequest));
